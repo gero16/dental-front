33ba8bac0f63f2e92788129868953e895a1171c2
geronimo <geronicola1696@gmail.com>
2024-03-22 21:00:40 -0300

    arreglos css en portada

--- src/Components/Index/ContenidoPortada/ContenidoPortada.jsx
+++ src/Components/Index/ContenidoPortada/ContenidoPortada.jsx
@@ -2,6 +2,7 @@ import "./ContenidoPortada.css"
 import Portada1 from "../../../../public/portada.png"
 import Portada2 from "../../../../public/portada-2.jpg"
 import logo2 from "../../../../public/logo-2.png"
+import { FaPhoneSquareAlt  } from 'react-icons/fa';
 
 const ContenidoPortada = () => {
     return (
@@ -20,12 +21,25 @@ const ContenidoPortada = () => {
                 <section className="portada-texto">
                     <h2 className="h2-portada-texto width-70"> Un dentista llega a la raiz del problema </h2>
                     <p className="width-70 p-portada-texto"> Atención personalizada para cada sonrisa </p>
-                    <ul className="lista-portada width-70" >
+                    
+                    
+
+                    <ul className="lista-portada width-70">
+                        <li className="flex-center-v lista-numero"> 
+                            <FaPhoneSquareAlt  size={19} />  
+                            <span className="span-portada-numero"> (598) 94 484 397  </span>  
+                    
+                        </li>
+                    </ul>
+
+                    <ul className="lista-portada lista-portada-2 width-70" >
+                        
                         <li className="white li-portada-texto"> Martes y Jueves 13:00 - 20:00 (Las Piedras) </li>
                         <li className="white  li-portada-texto"> Miercoles 13:00 - 20:00 (Montevideo) </li>
                         <li className="white  li-portada-texto"> Sabados 09:00 - 14:00 (Las Piedras) </li>
                     </ul>
-                <img src={logo2} className="logo-2-portada" alt="" />
+                    <img src={logo2} className="logo-2-portada" alt="" />
+
                 </section>
             </article>
         </>

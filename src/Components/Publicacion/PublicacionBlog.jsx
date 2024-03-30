e558936023b9ee06d170d9da45044a8812fce53b
geronimo <geronicola1696@gmail.com>
2024-03-30 18:42:27 -0300

    mejora limiteTexto

--- src/Components/Publicacion/PublicacionBlog.jsx
+++ src/Components/Publicacion/PublicacionBlog.jsx
@@ -7,26 +7,35 @@ import "./Publicacion.css"
 import { Link as Navigate, NavLink,} from "react-router-dom";
 
 const PublicacionBlog = ({data}) => {
-    console.log(data)
+    //console.log(data)
     
-    console.log(data.contenido[0].texto.length)
+    //console.log(data.contenido[0].texto.length)
 
     const claseCSS = data.titulo.length < 24 ? 'h2-publicacion-blog' : 'h2-publicacion-blog medium';
     const [width, setWidth] = useState(window.innerWidth);
 
+    let textoPubli = data.contenido[0].texto
+    let textoPubli2 =  data.contenido[1].texto
 
-    const limiteTexto = (texto, longitudMaxima) => {
+    const limiteTexto = (texto, longitudMaxima, texto2) => {
         // Verificar si el texto es más largo que la longitud máxima
         if (texto.length > longitudMaxima) {
           // Cortar el texto hasta la longitud máxima y agregar puntos suspensivos
           return texto.substring(0, longitudMaxima) + '...';
-        } else {
+        } 
+        if(texto.length < longitudMaxima && texto.length < 150) {
+            console.log("hola")
+            const nuevo = texto.substring(0, longitudMaxima) + texto2.substring(0, longitudMaxima) + "..."
+            return nuevo
+        }
+        else {
           // Si el texto no es más largo que la longitud máxima, devolver el texto original
           return texto;
         }
     }
 
-    const textoPubli = data.contenido[0].texto
+ 
+ 
 
     return (
         <> 
@@ -34,7 +43,13 @@ const PublicacionBlog = ({data}) => {
                 <img  src={ data.imagen } className="img-publicacion-blog" alt="" />
                 <section className="contenido-publicacion-blog"> 
                     <h2 className={ claseCSS }>  { data.titulo } </h2>
-                     <p> { width < 1500 ? limiteTexto(textoPubli, 350) :  limiteTexto(textoPubli, 500)}</p>
+                    
+                
+                        
+                         <p> { width < 1500 ? limiteTexto(textoPubli, 350, textoPubli2) :  limiteTexto(textoPubli, 500)}</p>
+                    
+                
+                    
                 </section>
 
             </NavLink>

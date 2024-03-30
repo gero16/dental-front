9ad0d4fdb9b2df88684106a7a8a628a7191138b2
geronimo <geronicola1696@gmail.com>
2024-03-30 15:45:58 -0300

    limitar cantidad de caracteres en PublicacionBlog

--- src/Components/Publicacion/PublicacionBlog.jsx
+++ src/Components/Publicacion/PublicacionBlog.jsx
@@ -8,20 +8,33 @@ import { Link as Navigate, NavLink,} from "react-router-dom";
 const PublicacionBlog = ({data}) => {
     console.log(data.url)
     
-    console.log(data.titulo.length)
+    console.log(data.contenido[0].texto.length)
 
     const claseCSS = data.titulo.length < 24 ? 'h2-publicacion-blog' : 'h2-publicacion-blog medium';
-    
+
+   
+    const limiteTexto = (texto, longitudMaxima) => {
+        // Verificar si el texto es más largo que la longitud máxima
+        if (texto.length > longitudMaxima) {
+          // Cortar el texto hasta la longitud máxima y agregar puntos suspensivos
+          return texto.substring(0, longitudMaxima) + '...';
+        } else {
+          // Si el texto no es más largo que la longitud máxima, devolver el texto original
+          return texto;
+        }
+    }
+
+    const textoPubli = data.contenido[0].texto
+
     return (
         <> 
            <NavLink to={`/blog/publicaciones/${ data.url }`}  className="publicacion-blog arcoiris">
                 <img  src={ data.imagen } className="img-publicacion-blog" alt="" />
                 <section className="contenido-publicacion-blog"> 
                     <h2 className={ claseCSS }>  { data.titulo } </h2>
-                     <p> { data.contenido[0].texto }</p>
+                     <p> { limiteTexto(textoPubli, 380)}</p>
                 </section>
 
- 
             </NavLink>
         </>
     )

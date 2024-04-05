166a251de62e02e9016f9de82d1f0a0481e16772
geronimo <geronicola1696@gmail.com>
2024-04-05 20:25:25 -0300

    link seleccionado en navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,18 +5,32 @@ import imagenMenu from "../../../public/menu.png"
 
 import { IoIosArrowDown } from "react-icons/io";
 
-import { Link as Navigate, NavLink,} from "react-router-dom";
+import { Link as Navigate, NavLink, useLocation ,} from "react-router-dom";
 import { useState } from "react";
 
 const Navbar = () => {
+    
+    const location = useLocation();
+    let { pathname } = location;
+    let url = pathname
+    console.log(url)
+    if(pathname.includes("/servicios/")) {
+     pathname = "/servicios"
+    }
+
 
     const [width, setWidth] = useState(window.innerWidth)
 
-    const [active, setActive] = useState(false)
-    const cambio = (valor) => {
-        setActive(valor)
+    const [selectedLink, setSelectedLink] = useState(pathname)
+
+
+    const handleLinkClick = (link) => {
         console.log(e)
-    }
+ 
+            setSelectedLink(link);
+    
+    };
+
     return (
         <> 
             <header>
@@ -52,30 +66,29 @@ const Navbar = () => {
                             ? <img src={ imagenMenu } alt="" className="img-menu" /> 
                             : <ul className="flex-center-center lista-navbar">
                             <li> 
-                                <NavLink to={`/`} > 
-                                    <span className="span-link "> Inicio </span>
+                                <NavLink to={`/`} className={selectedLink === '/' ? 'link-activo' : ''} onClick={(e) => handleLinkClick('/' )} > 
+                                    <span  className="span-link " > Inicio </span>
                                 </NavLink>
                             </li>
                             <li> 
-                                <NavLink to={`/nosotros`} > 
+                                <NavLink to={`/nosotros`} className={selectedLink === '/nosotros' ? 'link-activo' : ''} onClick={(e) => handleLinkClick('/nosotros', e)} > 
                                     <span className="span-link "> Nosotros </span>
                                 </NavLink>
                             </li>
                             <li> 
-                                <NavLink to={`/contacto`} > 
+                                <NavLink to={`/contacto`} className={selectedLink === '/contacto' ? 'link-activo' : ''} onClick={() => handleLinkClick('/contacto')} > 
                                     <span className="span-link "> Contacto </span>
                                 </NavLink>
                             </li>
                             <li className="li-servicios"> 
-                                <NavLink to={`/servicios/servicio-general`} > 
+                                <NavLink to={`/servicios/servicio-general`} className={selectedLink === '/servicios' ? 'link-activo' : ''} onClick={() => handleLinkClick('/servicios')} > 
                                     <span className="span-link"> 
                                             
                                          Servicios
                                         <IoIosArrowDown />
 
                                         <ul 
-                                            className="lista-servicios flex-column gap-10 inactive"
-                                            onClick={(e) => cambio(!active, e)}>
+                                            className="lista-servicios flex-column gap-10 inactive">
                                             <li> Servicio Periodontal </li>
                                             <li> Servicio General </li>
                                             <li> Servicio Cosmético </li>
@@ -88,24 +101,24 @@ const Navbar = () => {
                      
                             
                             <li> 
-                                <NavLink to={`/blog`} > 
-                                    <span className="span-link "> Blog </span>
+                                <NavLink to={`/blog`} className={selectedLink === '/blog' ? 'link-activo' : ''} onClick={() => handleLinkClick('/blog')} > 
+                                    <span className={"span-link"}> Blog </span>
                                 </NavLink>
                             </li>
                             <li> 
-                                <NavLink to={`/reservar-hora`} > 
+                                <NavLink to={`/reservar-hora`} className={selectedLink === '/reservar-hora' ? 'link-activo' : ''} onClick={() => handleLinkClick('/reservar-hora')}> 
                                     <span className="span-link "> Reservar Hora </span>
                                 </NavLink>
                             </li>
     
                             <li> 
-                                <NavLink to={`/iniciar-sesion`} > 
+                                <NavLink to={`/iniciar-sesion`} className={selectedLink === '/iniciar-sesión' ? 'link-activo' : ''} onClick={() => handleLinkClick('/iniciar-sesión')}> 
                                     <span className="span-link ">  Iniciar Sesión </span>
                                 </NavLink>
                             </li>
     
                             <li> 
-                                <NavLink to={`/admin/panel`} > 
+                                <NavLink to={`/admin/panel`} className={selectedLink === '/panel' ? 'link-activo' : ''} onClick={() => handleLinkClick('/panel')} > 
                                     <span className="span-link ">  Panel </span>
                                 </NavLink>
                             </li>

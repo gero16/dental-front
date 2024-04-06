c4ac9a76a3e9c4b57c4d5d5e4189f8b0c2964ad4
geronimo <geronicola1696@gmail.com>
2024-04-05 21:14:40 -0300

    actualizacion css panel

--- src/Components/Admin/Panel/Panel.jsx
+++ src/Components/Admin/Panel/Panel.jsx
@@ -6,26 +6,31 @@ const Panel = () => {
     return (
         <> 
         <Navbar> </Navbar>
-         <section className="section-panel flex-center-center fondo-blanco-img">
 
-            <NavLink to={`/admin/panel/administrar-publicaciones`} className="div-panel text-center" > 
-                <h2 className="h2-panel"> Administrar Publicaciones </h2>
-                <img 
-                    src="https://uem.edu.mx/wp-content/uploads/2021/01/6909-scaled-1024x732.jpg" 
-                    alt="" 
-                    className="img-panel"
-                    />
-            </NavLink> 
+        <section className="section-panel-big fondo-blanco-img flex-center-center">
 
-            <NavLink to={`/admin/panel/horas-disponibles`} className="div-panel text-center" > 
-                <h2 className="h2-panel"> Administrar Horas Disponibles  </h2>
-                <img 
-                    src="https://carrerasymas.com/wp-content/uploads/2022/05/20945988-scaled-1.jpg" 
-                    alt="" 
-                    className="img-panel"
-                    />
-            </NavLink> 
-         </section>
+            <section className="section-panel-small flex-center-center">
+
+                <NavLink to={`/admin/panel/administrar-publicaciones`} className="div-panel text-center" > 
+                    <h2 className="h2-panel h2-panel-1"> Administrar Publicaciones </h2>
+                    <img 
+                        src="https://uem.edu.mx/wp-content/uploads/2021/01/6909-scaled-1024x732.jpg" 
+                        alt="" 
+                        className="img-panel"
+                        />
+                </NavLink> 
+
+                <NavLink to={`/admin/panel/horas-disponibles`} className="div-panel text-center" > 
+                    <h2 className="h2-panel h2-panel-2"> Administrar Horas Disponibles  </h2>
+                    <img 
+                        src="https://carrerasymas.com/wp-content/uploads/2022/05/20945988-scaled-1.jpg" 
+                        alt="" 
+                        className="img-panel"
+                        />
+                </NavLink> 
+            </section>
+
+        </section>
         </>
     )
 }

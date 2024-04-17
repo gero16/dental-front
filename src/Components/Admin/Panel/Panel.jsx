20a44af27c816f43b5b3fad8d94a50f2315fe977
geronimo <geronicola1696@gmail.com>
2024-04-17 03:17:05 -0300

    mejora en panel

--- src/Components/Admin/Panel/Panel.jsx
+++ src/Components/Admin/Panel/Panel.jsx
@@ -1,32 +1,33 @@
 import { Link as Navigate, NavLink,} from "react-router-dom";
 import "./Panel.css"
 import Navbar from "../../Navbar/Navbar";
+import Imagen1 from "../../../../public/campana.png"
+import Imagen2 from "../../../../public/calendario.png"
+import Imagen3 from "../../../../public/reloj.png"
 
 const Panel = () => {
     return (
         <> 
         <Navbar> </Navbar>
 
-        <section className="section-panel-big fondo-blanco-img flex-center-center">
+        <section className="section-panel-big fondo-blanco-img flex-column-center text-center gap-30">
 
-            <section className="section-panel-small flex-center-center">
+            <h1 className="h1-panel verde-principal">Panel de Administracion </h1>
+            <section className="section-panel-small flex-center-center text-center">
 
                 <NavLink to={`/admin/panel/administrar-publicaciones`} className="div-panel div-panel-1" > 
-                    <h2 className="h2-panel h2-panel-1"> Administrar Publicaciones </h2>
-                    <img 
-                        src="https://uem.edu.mx/wp-content/uploads/2021/01/6909-scaled-1024x732.jpg" 
-                        alt="" 
-                        className="img-panel"
-                        />
+                    <h2 className="h2-panel h2-panel-1">  Publicaciones </h2>
+                    <img src={Imagen1} alt=""  className="img-panel" />
                 </NavLink> 
 
                 <NavLink to={`/admin/panel/horas-disponibles`} className="div-panel div-panel-2 text-center" > 
-                    <h2 className="h2-panel h2-panel-2"> Administrar Horas Disponibles  </h2>
-                    <img 
-                        src="https://carrerasymas.com/wp-content/uploads/2022/05/20945988-scaled-1.jpg" 
-                        alt="" 
-                        className="img-panel"
-                        />
+                    <h2 className="h2-panel h2-panel-2">  Horarios Disponibles  </h2>
+                    <img src={Imagen3} alt=""  className="img-panel"  />
+                </NavLink> 
+
+                <NavLink to={`/admin/panel/horas-disponibles`} className="div-panel div-panel-2 text-center" > 
+                    <h2 className="h2-panel h2-panel-2"> Ver Agendados  </h2>
+                    <img src={Imagen2} alt=""  className="img-panel"  />
                 </NavLink> 
             </section>
 

52faa84f5bb5e1d10f2366432fe884e06657ae90
geronimo <geronicola1696@gmail.com>
2024-04-18 04:57:04 -0300

    estados en ubicaciones para ver un solo mapa

--- src/Components/Ubicacion/Ubicaciones.jsx
+++ src/Components/Ubicacion/Ubicaciones.jsx
@@ -1,42 +1,57 @@
 import "./Ubicacion.css";
 import Mapa from "../Mapa/Mapa.jsx";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const Ubicaciones = () => {
     const ubiTresCruces = [-34.89578868112877, -56.16680986705166];
     const ubiLasPiedras = [-34.728071363547315, -56.21382609708981];
 
     const [width, setWidth] = useState(window.innerWidth);
+    const [estilo, setEstilo] = useState([]);
+    const [mostrarUbi1, setMostrarUbi1] = useState(true);
+    const [ubicacion, setUbicacion] = useState(ubiTresCruces)
+    const [mapaRenderizado, setMapaRenderizado] = useState(false);
 
-    let estilo;
+    useEffect(() => {
+    
+        setUbicacion(mostrarUbi1 ? ubiTresCruces : ubiLasPiedras);
+        // El mapa se ha actualizado, establecer mapaRenderizado en true después de un breve retraso
+        const timeout = setTimeout(() => {
+            setMapaRenderizado(true);
+        }, 200);
+
+        // Limpiar el timeout si el componente se desmonta antes de que se complete el renderizado del mapa
+        return () => clearTimeout(timeout);
+    }, [mostrarUbi1]);
 
 
-    if (width <= 950) {
-        estilo = [300, 300, 0];
-    } 
-    if (width > 950 && width <= 1200) {
-        estilo = [220, 320, 0];
-    } 
-    if (width > 1200 && width <= 1560) {
-        estilo = [320, 420, 0];
-    }
-    if (width > 1560) {
-        estilo = [500, 700, 0];
-    }
+    useEffect(() => {
+        // Actualizar el estilo del mapa según el ancho de la ventana - Sin useEffect habia daba error
+        if (width <= 450 && width < 800)  setEstilo([320, 400, 0]);
+        if (width >= 800 && width < 1200) setEstilo([400, 500, 0]);
+        if (width >= 1200 && width <= 1500) setEstilo([450, 550, 0]);
+        if (width >= 1560) setEstilo([550, 650, 0]);
+        
+    }, [width]);
+
+  
+
+    const cambiarUbi = (estado) => {
+        setMapaRenderizado(false); // Restablecer el estado del mapa renderizado al cambiar de ubicación
+        setMostrarUbi1(estado);
+    };
 
     return (
-        <section className="section-ubicacion flex-column-center">
-            <article className="flex-column-center article-ubicacion">
-                <h2 className="h2-clinica"> Clinica - Las Piedras </h2>
-                <span className="mb-5"> Torre García 536 </span>
-                <Mapa ubicacion={ ubiLasPiedras } clinica={"las-piedras"} style={ estilo }> </Mapa>
-            </article> 
+        <section className="section-ubicacion flex-column-center m-auto">
+
+            <h3 onClick={() => cambiarUbi(!mostrarUbi1)} className="mostrar-ubi"> Cambiar Ubicación </h3>
 
             <article className="flex-column-center article-ubicacion">
-                <h2 className="h2-clinica"> Clinica - Tres Cruces </h2>
-                <span className="mb-5"> Victor Haedo 2322 Apto 109 </span>
-                <Mapa ubicacion={ ubiTresCruces } clinica={"tres-cruces"} style={ estilo } > </Mapa>
+                <h2 className="h2-clinica">{mostrarUbi1 ? 'Clinica - Tres Cruces' : 'Clinica - Las Piedras'}</h2>
+                <span className="mb-5">{mostrarUbi1 ? 'Victor Haedo 2322 Apto 109' : 'Torre García 536'}</span>
+                { mapaRenderizado && <Mapa ubicacion={ubicacion} clinica={mostrarUbi1 ? "tres-cruces" : "las-piedras"} style={estilo} /> }
             </article>
+
         </section>
     );
 };

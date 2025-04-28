c1656e7fd4c91f3ad9355bf3e20e80af21847edc
gero <geronicola1696@gmail.com>
2025-04-28 14:22:12 -0300

    arreglos css

--- src/Components/Contacto/Contacto.jsx
+++ src/Components/Contacto/Contacto.jsx
@@ -33,79 +33,68 @@ const Contacto = () => {
 
             <section className="portada-contacto"> </section>
 
-            <section className="fondo-blanco-img flex-column gap-30">
-
-                <section className="section-contacto-header">
-
-                    <section className="contacto-header-2 container flex-column-center gap-10">
-
-                        <section className="flex-column width-70">
+            <section className="fondo-blanco-img flex-column-center gap-30 mt-5p">
+                <article className="article-info-consulta2 flex-column-center gap-20 text-center">
+                <section className="flex-column-center gap-30 section-contacto-header"> 
+                        <section className="sections-info-consulta2 flex-column-center br p-20"> 
                             <h1 className="titulo-contacto"> Pongase en  Contacto </h1>
-                            <p className="p-header-contacto"> Envíenos un correo electrónico aquí: info@globalsystemdental.com o complete este formulario 
+
+                            <p className="p-header-contacto"> Puedes enviarnos un correo electrónico aquí: info@globalsystemdental.com o complete este formulario  
                                 y nos pondremos en contacto con usted en las próximas 24 horas.
                             </p>
                         </section>
 
-                        <section className="flex-column-center-v">
-                            <h3> Llame para una cita </h3>
-                            <h2 className="h2-numero-contacto flex-center-v gap-10"> <FaPhoneSquareAlt  size={20} />   + (598) 94 484 397 </h2>
-                        </section>
-
-                    </section>
-
-                        
-                </section>
-
-                <section className="container">
-                    
-                        <section className="flex section-contacto-formulario">
-
-                            <form action="#" className="formulario-contacto">
-                                <ul className="ul-formulario-contacto">
-                                    <li> <h3 className="h3-contacto"> ¡Envienos su consulta! </h3> </li>
-                            
-                                    <li className="li-formulario-contacto">
-                                        <label htmlFor="">Nombre </label>
-                                        <input type="text" />
-                                    </li>
-                                    <li  className="li-formulario-contacto">
-                                        <label htmlFor=""> Apellido  </label>
-                                        <input type="text" />
-                                    </li>
-                                    <li  className="li-formulario-contacto">
-                                        <label htmlFor=""> Correo Electronico </label>
-                                        <input type="text" />
-                                    </li>
-                                    <li  className="li-formulario-contacto">
-                                        <label htmlFor=""> Titulo </label>
-                                        <input type="text" />
-                                    </li>
-                                    <li  className="li-formulario-contacto">
-                                        <label htmlFor=""> Mensaje </label>
-                                        <textarea type="text" />
-                                    </li>
-
-                                    <span className="btn-enviar-contacto text-center"> Enviar </span>
-                                </ul>
-                            </form>
-
-                            <section className="img-contacto">      
-                
+                        <section className="flex-column gap-30"> 
+                            <section className="sections-info-consulta2 br p-20"> 
+                                <h2 className="info-consulta2-h2"> LLámenos </h2> 
+                                <h2 className="h2-numero info-consulta2-h2"> (598) 94 484 397 </h2>
+                                <p> ¡Estamos listos para ayudarlo a lograr una salud bucal increíble! </p>
                             </section>
-                            
-
                         </section>
-                    
-                
-                    <section className="section-ubicaciones flex-column">
-
-                       <Ubicaciones> </Ubicaciones>
-                       
-                    
                     </section>
+
+                    <Ubicaciones> </Ubicaciones>
+                </article>
+
+          
+                   
+                <section className="flex section-contacto-formulario">
+
+                    <form action="#" className="formulario-contacto">
+                        <ul className="ul-formulario-contacto">
+                            <li> <h3 className="h3-contacto"> ¡Envienos su consulta! </h3> </li>
+
+                            <li className="li-formulario-contacto">
+                                <label htmlFor="">Nombre </label>
+                                <input type="text" />
+                            </li>
+                            <li  className="li-formulario-contacto">
+                                <label htmlFor=""> Apellido  </label>
+                                <input type="text" />
+                            </li>
+                            <li  className="li-formulario-contacto">
+                                <label htmlFor=""> Correo Electronico </label>
+                                <input type="text" />
+                            </li>
+                            <li  className="li-formulario-contacto">
+                                <label htmlFor=""> Titulo </label>
+                                <input type="text" />
+                            </li>
+                            <li  className="li-formulario-contacto">
+                                <label htmlFor=""> Mensaje </label>
+                                <textarea type="text" />
+                            </li>
+
+                            <span className="btn-enviar-contacto text-center"> Enviar </span>
+                        </ul>
+                    </form>
+
                  
-                
+
+
                 </section>
+
+
 
             </section>
          
--- src/Components/Index/Index.jsx
+++ src/Components/Index/Index.jsx
@@ -26,31 +26,38 @@ const Index = () => {
 
         
             <IndexServicios> </IndexServicios> 
-        
-            <IndexSaludDental> </IndexSaludDental>
+        {
+            /* 
+             <IndexSaludDental> </IndexSaludDental>
 
             <IndexInfoConsulta> </IndexInfoConsulta>
 
             
             <IndexOpinionCliente> </IndexOpinionCliente>
+            */
+        }
+           
 
-            <article className="article-info-consulta2 flex-column gap-20 text-center">
 
-                <section className="sections-info-consulta2"> 
-                    <h2 className="info-consulta2-h2"> LLámenos </h2> 
-                    <h2 className="h2-numero info-consulta2-h2"> (598) 94 484 397 </h2>
-                    <p> ¡Estamos listos para ayudarlo a lograr una salud bucal increíble! </p>
+            <article className="article-info-consulta2 flex-column-center gap-20 text-center">
+
+                <section className="flex-column gap-30"> 
+                    <section className="sections-info-consulta2 br p-20"> 
+                        <h2 className="info-consulta2-h2"> LLámenos </h2> 
+                        <h2 className="h2-numero info-consulta2-h2"> (598) 94 484 397 </h2>
+                        <p> ¡Estamos listos para ayudarlo a lograr una salud bucal increíble! </p>
+                    </section>
+
+                    <section className="sections-info-consulta2 br p-20"> 
+                        <h2 className="info-consulta2-h2"> Solicite su cita hoy </h2>
+                        <p> Por dudas o consultas </p>
+                        <p className="btn-consultar"> <a href="/contacto"> Consultar </a> </p>
+                    </section>
+
                 </section>
 
-                <section className="sections-info-consulta2"> 
-                    <h2 className="info-consulta2-h2"> Solicite su cita hoy </h2>
-                    <p> Por dudas o consultas </p>
-                    <p className="btn-consultar"> <a href="/contacto"> Consultar </a> </p>
-                </section>
-
+                <Ubicaciones> </Ubicaciones>
             </article>
-
-            <Ubicaciones> </Ubicaciones>
 
 
             <Whatsapp> </Whatsapp>
